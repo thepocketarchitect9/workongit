22ae5f2a4c5e4d5daaf0a732a2d08e996db8b250
Anieka Scholtz <anieka1@gmail.com>
2019-09-04 12:31:12 +0200

    Check if number 1 is less than number 2

--- division.js
+++ division.js
@@ -1,3 +1,11 @@
 function division(numb1, numb2) {
-  return numb1/numb2;
+  var result;
+  if(numb1 < numb2)
+  {
+    result = numb1/numb2
+  }
+  else{
+    result = numb2/numb1;
+  }
+  return result;
 }
